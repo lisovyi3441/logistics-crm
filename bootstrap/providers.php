eb4949866c1494fddf107442addedd62db8b8860
Maks Lisovyi <lisovyi3441@gmail.com>
2026-03-04 21:36:50 +0000

    fix: conditionally register telescope to prevent production build crash

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -4,5 +4,4 @@ return [
     App\Providers\AppServiceProvider::class,
     App\Providers\FortifyServiceProvider::class,
     App\Providers\RoutingServiceProvider::class,
-    App\Providers\TelescopeServiceProvider::class,
 ];
