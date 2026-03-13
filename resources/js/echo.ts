306ec47d4b34d71e4127e0684832ed708f9ecb10
Maks Lisovyi <lisovyi3441@gmail.com>
2026-03-13 18:49:51 +0000

    fix: assign Echo to window object for global access

--- resources/js/echo.ts
+++ resources/js/echo.ts
@@ -3,7 +3,7 @@ import Pusher from 'pusher-js';
 
 window.Pusher = Pusher;
 
-const echo = new Echo({
+window.Echo = new Echo({
     broadcaster: 'reverb',
     key: import.meta.env.VITE_REVERB_APP_KEY,
     wsHost: import.meta.env.VITE_REVERB_HOST,
@@ -12,5 +12,3 @@ const echo = new Echo({
     forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? 'https') === 'https',
     enabledTransports: ['ws', 'wss'],
 });
-
-export default echo;
